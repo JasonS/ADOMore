df4a088ff38a0c211e51aa4f8da80748ce1013c2
Chad Burggraf <chad.burggraf@gmail.com>
2012-12-17 10:07:45 -0700

    Port tests to NUnit for compatibility with MonoDevelop.

--- Source/ADOMore.Test/SQLTests.cs
+++ Source/ADOMore.Test/SQLTests.cs
@@ -6,18 +6,14 @@
     using System.Data;
     using System.Data.SqlClient;
     using System.IO;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NUnit.Framework;
 
-    [TestClass]
+    [TestFixture]
     public class SQLTests
     {
-        private static string connectionString;
-
-        [TestMethod]
+        [Test]
         public void CanCreateRecord()
         {
-            EnsureConnectionString();
-
             TestClass instance = new TestClass()
             {
                 SetGuid = Guid.NewGuid(),
@@ -43,7 +39,7 @@
                 SetNullTestType = TestType.Four | TestType.Eight
             };
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(Setup.ConnectionString))
             {
                 connection.Open();
                 CreateRecord(instance, connection);
@@ -51,12 +47,10 @@
             }
         }
 
-        [TestMethod]
+        [Test]
         public void TestDicitionaryExtenstion()
         {
-            EnsureConnectionString();
-            
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(Setup.ConnectionString))
             {
                 connection.Open();
 
@@ -67,18 +61,18 @@
             }
         }
 
-        [TestMethod]
+        [Test]
         public void TestTableExists()
         {
             int rowCount = 0;
             SqlDataReader reader = null;
-            EnsureConnectionString();
+            
             const string Sql =
 @"SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Test]') AND type = N'U'";
 
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(Setup.ConnectionString))
                 {
                     connection.Open();
 
@@ -137,145 +131,6 @@
                     command = null;
                 }
             }
-        }
-
-        [AssemblyInitialize]
-        public static void Bootstrap(TestContext testContext)
-        {
-            EnsureConnectionString();
-            Stream stream = null;
-            SqlCommand command = null;
-            string createSql = string.Empty;
-            string filePath = string.Empty;
-            SqlConnectionStringBuilder masterConn = new SqlConnectionStringBuilder(connectionString);
-
-            filePath = masterConn.AttachDBFilename;
-            masterConn.AttachDBFilename = string.Empty;
-            masterConn.InitialCatalog = "master";
-            createSql = string.Format(CultureInfo.InvariantCulture, "CREATE DATABASE [Test] ON (NAME=N'Test', FILENAME='{0}')", filePath);
-
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(masterConn.ConnectionString))
-                {
-                    connection.Open();
-                    Destroy(connection);
-                    command = connection.CreateCommand();
-                    command.CommandType = CommandType.Text;
-                    command.CommandText = createSql;
-                    command.ExecuteNonQuery(); ;
-                }
-            }
-            finally
-            {
-                if (command != null)
-                {
-                    command = null;
-                }
-            }
-
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    stream = typeof(SQLTests).Assembly.GetManifestResourceStream("ADOMore.Test.Schema.sql");
-
-                    using (StreamReader reader = new StreamReader(stream))
-                    {
-                        command = connection.CreateCommand();
-                        command.CommandType = CommandType.Text;
-                        command.CommandText = reader.ReadToEnd();
-                        command.ExecuteNonQuery();
-                    }
-                }
-            }
-            finally
-            {
-                if (stream != null)
-                {
-                    stream.Dispose();
-                    stream = null;
-                }
-                if (command != null)
-                {
-                    command.Dispose();
-                    command = null;
-                }
-            }
-        }
-
-        [AssemblyCleanup]
-        public static void Destroy()
-        {
-            EnsureConnectionString();
-            SqlConnectionStringBuilder masterConn = new SqlConnectionStringBuilder(connectionString);
-            masterConn.AttachDBFilename = string.Empty;
-            masterConn.InitialCatalog = "master";
-
-            using (SqlConnection connection = new SqlConnection(masterConn.ConnectionString))
-            {
-                connection.Open();
-                Destroy(connection);
-            }
-        }
-
-        private static void Destroy(SqlConnection connection)
-        {
-            const string DropSql =
-@"IF EXISTS (SELECT * FROM sys.databases WHERE [name] = 'Test')
-BEGIN
-    BEGIN TRY
-        ALTER DATABASE [Test] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    END TRY
-    BEGIN CATCH
-    END CATCH
-    DROP DATABASE [Test];
-END";
-
-            using(SqlCommand command = connection.CreateCommand())
-            {
-                command.CommandType = CommandType.Text;
-                command.CommandText = DropSql;
-                command.ExecuteNonQuery();
-            }
-        }
-
-        private static void EnsureConnectionString()
-        {
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                var path = ResolvePath((Path.GetRandomFileName().Replace(".", string.Empty) + ".mdf"));
-                connectionString = string.Format(CultureInfo.InvariantCulture, @"Server=(localdb)\v11.0;AttachDbFileName={0};Initial Catalog=Test;Integrated Security=true", path);
-            }
-        }
-
-        private static string ResolvePath(string path)
-        {
-            const string DataDirectory = "|DataDirectory|";
-
-            if (string.IsNullOrWhiteSpace(path))
-            {
-                path = AppDomain.CurrentDomain.BaseDirectory;
-            }
-
-            if (path.StartsWith(DataDirectory, StringComparison.OrdinalIgnoreCase))
-            {
-                path = path.Substring(DataDirectory.Length);
-                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", path);
-            }
-
-            if (!Path.IsPathRooted(path))
-            {
-                path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path));
-            }
-
-            if (!path.StartsWith(AppDomain.CurrentDomain.BaseDirectory, StringComparison.OrdinalIgnoreCase))
-            {
-                throw new ArgumentException("path must be within the current application directory.", "path");
-            }
-
-            return path;
         }
 
         private void AssertGetTestInstanceFromDatabase(TestClass instance, SqlConnection connection)
